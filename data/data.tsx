08027053d5e6d3e51429c5ec8f00b27111ef50a1
divine <divineobi07@gmail.com>
2023-09-18 21:57:02 +0100

    images data

---
+++ data/data.tsx
@@ -0,0 +1,188 @@
+export const data: {
+  id: number;
+  title: string;
+  blurHash: string;
+  pic: string;
+}[] = [
+  {
+    id: 0,
+    title: "a lone person walking in the middle of a desert",
+    blurHash: "LRGQatEN0$xZI?xFs.WCNHs.s.R+",
+    pic: "https://images.unsplash.com/photo-1682687220989-cbbd30be37e9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MXwxfGFsbHwxfHx8fHx8Mnx8MTY5NTA1MTQ1OXw&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 1,
+    title: "woman taking photo",
+    blurHash: "LzHxyp00SNx]x]M{t7RjWBogV@j[",
+    pic: "https://images.unsplash.com/photo-1566210006746-4363a0f45280?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyfHx8fHx8Mnx8MTY5NTA1MTQ1OXw&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 2,
+    title: "a building with stone steps leading up to a door",
+    blurHash: "LLI}bBIU-;4n58V@kCae8^xvRj-;",
+    pic: "https://images.unsplash.com/photo-1463130456064-77fda7f96d6b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGElMjBidWlsZGluZyUyMHdpdGglMjBzdG9uZSUyMHN0ZXBzJTIwbGVhZGluZyUyMHVwJTIwdG8lMjBhJTIwZG9vcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 3,
+    title: "a bowl of food sitting on top of a wooden table",
+    blurHash: "LIEnnm-;?Zkq^if+-nNt0fRisRE1",
+    pic: "https://images.unsplash.com/photo-1414438992182-69e404046f80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGElMjBidWlsZGluZyUyMHdpdGglMjBzdG9uZSUyMHN0ZXBzJTIwbGVhZGluZyUyMHVwJTIwdG8lMjBhJTIwZG9vcnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    id: 4,
+    title: "a woman laying on the ground with her legs crossed",
+    blurHash: "LD8|6Ft65RNHR*oexZWW0zR+-ns:",
+    pic: "https://images.unsplash.com/photo-1495482336510-f7716cc28940?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8YSUyMHdvbWFufGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 5,
+    title: "a woman standing in front of a rock formation",
+    blurHash: "LGDbye^i0K58yDIVVr.7yDE2Mx%L",
+    pic: "https://images.unsplash.com/photo-1682687221080-5cb261c645cb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MXwxfGFsbHw2fHx8fHx8Mnx8MTY5NTA1MTQ1OXw&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 6,
+    title: "a red truck parked in front of a building",
+    blurHash: "LOGbVM~WV@axIp%MoJV@4.NHofRP",
+    pic: "https://images.unsplash.com/photo-1517481705099-72660903e569?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8YSUyMHJlZCUyMHRydWNrJTIwcGFya2VkJTIwaW4lMjBmcm9udCUyMG9mJTIwYSUyMGJ1aWxkaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 8,
+    title: "happy birthday",
+    blurHash: "LFF~Ef8^tnRO9Fa$a}WA.TtSV=WX",
+    pic: "https://images.unsplash.com/photo-1464347601390-25e2842a37f7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YSUyMHJlZCUyMHRydWNrJTIwcGFya2VkJTIwaW4lMjBmcm9udCUyMG9mJTIwYSUyMGJ1aWxkaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 9,
+    title: "a bicycle parked on the side of a street",
+    blurHash: "LFD]9#IU0KM{?H%29FIUI;ozIURj",
+    pic: "https://images.unsplash.com/photo-1571068316344-75bc76f77890?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YmljeWNsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=600&q=60",
+  },
+  {
+    id: 10,
+    title: "a group of animals walking across a grass covered field",
+    blurHash: "L~KIg;NHR,of};aejabb%Jr@n*W;",
+    pic: "https://images.unsplash.com/photo-1682687982204-f1a77dcc3067?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MXwxfGFsbHwxfHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 11,
+    title: "a leaf that is sitting on the hood of a car",
+    blurHash: "LB97eGWZE0S6~pE2IUM|-:NLs,IB",
+    pic: "https://images.unsplash.com/photo-1694986811068-74034f1f9b82?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyfHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 12,
+    title: "a person sitting on a bench in front of paintings",
+    blurHash: "LgGITAWBaeae-;fQWBj[01ofWVof",
+    pic: "https://images.unsplash.com/photo-1695010801799-3543da0c0059?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwzfHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 13,
+    title: "a mountain range is reflected in the still water of a lake",
+    blurHash: "LnGb-hNGRjt7_NayRjof%#jsa}WC",
+    pic: "https://images.unsplash.com/photo-1695010123518-2cc8ceac2e54?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHw0fHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 14,
+    title: "Asian girl taking a photo with the ​camera",
+    blurHash: "LzHxyp00SNx]x]M{t7RjWBogV@j[",
+    pic: "https://images.unsplash.com/photo-1566210006746-4363a0f45280?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHw1fHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 15,
+    title:
+      "Two autistic friends sitting outside using stim toys and laughing at their phones",
+    blurHash: "LMGbbQ-qD=tQ9W9Zw}xCx]R*S0t7",
+    pic: "https://images.unsplash.com/photo-1687360441027-27e70655b27e?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MXwxfGFsbHw2fHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 16,
+    title: "a building with stone steps leading up to a door",
+    blurHash: "LLI}bBIU-;4n58V@kCae8^xvRj-;",
+    pic: "https://images.unsplash.com/photo-1682917595484-41b62dc54320?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHw3fHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 17,
+    title: "a bowl of food sitting on top of a wooden table",
+    blurHash: "LIEnnm-;?Zkq^if+-nNt0fRisRE1",
+    pic: "https://images.unsplash.com/photo-1693922876041-7fb22341f33a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHw4fHx8fHx8Mnx8MTY5NTA1MzUzN3w&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 18,
+    title: "an aerial view of a desert with rocks and sand",
+    blurHash: "LpE.-8s:axo0~qoeWBjZ?vaye.ay",
+    pic: "https://images.unsplash.com/photo-1682687220067-dced9a881b56?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MXwxfGFsbHwxMXx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 19,
+    title: "a woman standing on top of a sandy beach",
+    blurHash: "LPDJ6ZM_RPj[.TRjRij[IpbcWBa}",
+    pic: "https://images.unsplash.com/photo-1694536591647-a04dc2e141ea?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwxNXx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 20,
+    title: "a row of old books sitting on a shelf",
+    blurHash: "LLD+#lo}Mx%gD%WBt7Rj0KV@t7RP",
+    pic: "https://images.unsplash.com/photo-1694180846895-5d97f0b4ad3b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwxN3x8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 21,
+    title: "a tall building with a street light in the middle of it",
+    blurHash: "LC7KJ2WB0fof?HWBD*t7OsRk#lxa",
+    pic: "https://images.unsplash.com/photo-1694032073070-c54e65e77bbd?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwxOHx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 22,
+    title: "a man in a black cloak standing in the desert",
+    blurHash: "L58D@H-oxtV[LgXSWCay9FNwM{t7",
+    pic: "https://images.unsplash.com/photo-1694861651735-555e68428c72?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwxOXx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 23,
+    title: "a room with a glass floor and a view of the water",
+    blurHash: "LLC6Z8bI9FM{%LWXogay0Mt7-:ay",
+    pic: "https://images.unsplash.com/photo-1694809564205-d0df664ce86d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyMHx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 24,
+    title: "a person swimming in the ocean with a mountain in the background",
+    blurHash: "LsEgLFVpWBWZ_NNMW=a_%NNgjZn~",
+    pic: "https://images.unsplash.com/photo-1682695797873-aa4cb6edd613?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MXwxfGFsbHwyMXx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 25,
+    title: "an elephant crossing the road in front of a car",
+    blurHash: "LeB|siozM{ay?wkCRjj[x^aebHkC",
+    pic: "https://images.unsplash.com/photo-1694793435731-bce6e06c2cd1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyMnx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 26,
+    title: "two drinks sitting on a table next to a vase of flowers",
+    blurHash: "LEG[vQ4mS$xt00M_-;RP01D%RiNG",
+    pic: "https://images.unsplash.com/photo-1694865045254-39ae2937cf1d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyNHx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 27,
+    title: "a view of the ocean from the top of a hill",
+    blurHash: "LMFh|,_3IoM{~qjFM{j[%MM{xat7",
+    pic: "https://images.unsplash.com/photo-1694975452344-51e6ed6f59a2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyNXx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 28,
+    title: "a large body of water surrounded by mountains",
+    blurHash: "L+Gv3FoJR+S5?wj]jsoLXTbHs.oJ",
+    pic: "https://images.unsplash.com/photo-1694687530306-e334f8ffa84a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyN3x8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 29,
+    title: "a close up of a pile of wood",
+    blurHash: "L4K8@29c0~t6NHxsxDaL0$kB%0r?",
+    pic: "https://images.unsplash.com/photo-1694675042062-f73713a6a122?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyOHx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+  {
+    id: 30,
+    title: "a black car is parked in a parking lot",
+    blurHash: "LDD+k_^kbbIU=|-:ofIV0MWVoMoe",
+    pic: "https://images.unsplash.com/photo-1694676518566-874b80d2d160?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wzMjA2NDJ8MHwxfGFsbHwyOXx8fHx8fDJ8fDE2OTUwNTM1Mzd8&ixlib=rb-4.0.3&q=80&w=400",
+  },
+];
